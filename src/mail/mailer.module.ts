f1b2e74b33ca1f9d22d979b2041164b9ccfa9e29
luisaguerrero0801 <luisaguerrero0801@gmail.com>
2025-09-28 01:19:14 -0500

    Configuración de API de google gmail para correos

--- src/mail/mailer.module.ts
+++ src/mail/mailer.module.ts
@@ -1,8 +1,8 @@
 import { Global, Module } from '@nestjs/common';
 import { MailerService } from './mailer.service';
-import * as nodemailer from 'nodemailer';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
+import { createGmailTransporter } from './gmail.transporter';
 
 @Global()
 @Module({
@@ -13,7 +13,7 @@ import { JwtModule } from '@nestjs/jwt';
       inject: [ConfigService],
       useFactory: async (config: ConfigService) => ({
         secret: config.get<string>('JWT_SECRET'),
-        signOptions: { expiresIn: '1d' }, // para tokens de verificación
+        signOptions: { expiresIn: '1d' },
       }),
     }),
   ],
@@ -21,19 +21,8 @@ import { JwtModule } from '@nestjs/jwt';
     {
       provide: 'MAILER_TRANSPORTER',
       inject: [ConfigService],
-      useFactory: (config: ConfigService) =>
-        nodemailer.createTransport({
-          host: config.get<string>('EMAIL_HOST'),
-          port: Number(config.get<number>('EMAIL_PORT')),
-          secure: false,
-          requireTLS: true,
-          auth: {
-            user: config.get<string>('EMAIL_USER'),
-            pass: config.get<string>('EMAIL_PASS'),
-          },
-          tls: { rejectUnauthorized: false },
-          logger: true,
-        }),
+      useFactory: async (config: ConfigService) =>
+        await createGmailTransporter(config),
     },
     MailerService,
   ],
