66e55ba7b3ee432b3a66b033b055fd1196a5c6d8
luisaguerrero0801 <luisaguerrero0801@gmail.com>
2025-09-27 23:18:39 -0500

    usuario

--- src/usuarios/usuarios.service.ts
+++ src/usuarios/usuarios.service.ts
@@ -117,6 +117,7 @@ export class UsuariosService {
         rol: { nombreRol: 'Doctor' },
         estado: 'Activo',
       },
+      select: ['idUsuario', 'nombresUsuario', 'apellidosUsuario', 'estado'],
     });
   }
 
